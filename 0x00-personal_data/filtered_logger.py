70703a7ca1be09a384872e8cd8dd0f1f2d423e4e
dev-tch <tijeni.chaabeni@gmail.com>
2024-05-16 03:42:27 +0000

    change message of logging formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 """
-module with one function filter_datum
+module with:
+* one function filter_datum
+* class RedactingFormatter
+
 """
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str],
@@ -30,3 +34,23 @@ def filter_datum(fields: List[str],
     # we have to add '=' to replacement because first group contain only field
     replacement = r'\1={}{}'.format(redaction, separator)
     return re.sub(pattern, replacement, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ constructor method"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self._fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ string representation of Loging record"""
+        record.msg = filter_datum(self._fields, self.REDACTION,
+                                  record.msg, self.SEPARATOR)
+        return super().format(record)
