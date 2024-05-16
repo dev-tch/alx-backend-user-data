467b676d8dac520fc4060587adcf65ea68d2df11
dev-tch <tijeni.chaabeni@gmail.com>
2024-05-16 19:31:44 +0000

    check  hashed password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,3 +6,9 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """ hash password"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password):
+    """ validates password with hashed value"""
+    hashed = hash_password(password)
+    return bcrypt.checkpw(password.encode('utf-8'), hashed)
