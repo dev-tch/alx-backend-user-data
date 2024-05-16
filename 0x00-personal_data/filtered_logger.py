8d277cd38b0e074a7dd6a6717baea1341e4cada2
dev-tch <tijeni.chaabeni@gmail.com>
2024-05-16 17:32:16 +0000

    fix get_db function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -76,11 +76,12 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """ connect to mysql database and return MySQLConnection object"""
-    config = {
-        'user': os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
-        'password': os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
-        'host': os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
-        'database': os.getenv('PERSONAL_DATA_DB_NAME')
-    }
-    con_obj = mysql.connector.connection.MySQLConnection(**config)
-    return con_obj
+    user = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    database = os.getenv('PERSONAL_DATA_DB_NAME')
+    cnx = mysql.connector.connection.MySQLConnection(user=user,
+                                                     password=password,
+                                                     host=host,
+                                                     database=database)
+    return cnx
