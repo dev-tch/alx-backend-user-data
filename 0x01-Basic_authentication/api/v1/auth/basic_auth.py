fa43d3bf7a3aba58da6edf39bbc087ab5f51888e
dev-tch <tijeni.chaabeni@gmail.com>
2024-05-21 19:44:32 +0000

    decode auth base64  to byte then to utf8 str

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ module for BASIC authentication method"""
 from api.v1.auth.auth import Auth
 from flask import request
+import base64
 
 
 class BasicAuth(Auth):
@@ -16,3 +17,20 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """ base64 decode"""
+        part_base64 = base64_authorization_header
+        if part_base64 is None:
+            return None
+        if not isinstance(part_base64, str):
+            return None
+        try:
+            byte_decoded = base64.b64decode(part_base64)
+            # convert byte to string UTF-8
+            utf8_str = byte_decoded.decode('UTF-8')
+            return utf8_str
+        except (UnicodeDecodeError, base64.binascii.Error):
+            return None
