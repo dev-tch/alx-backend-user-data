dbb26472987741827993139d887729a09c75f771
dev-tch <tijeni.chaabeni@gmail.com>
2024-05-23 19:23:57 +0000

    get user with cookie method

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 from api.v1.auth.auth import Auth
 import uuid
 import os
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -23,3 +24,16 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ returns a User instance based on a cookie value"""
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+        try:
+            return User.get(user_id)
+        except KeyError:
+            return None
