8a3d9601ba2ea024af88a5959021eeef7eec2f1f
dev-tch <tijeni.chaabeni@gmail.com>
2024-05-29 19:39:15 +0000

    endressource to register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Flask application
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+auth = Auth()
 
 
 @app.route('/', strict_slashes=False)
@@ -12,6 +14,18 @@ def hello_hbnb():
     return message Bienvenue when web server invoking router '/'
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register_user():
+    """ api to register new user"""
+    email = request.form['email']
+    password = request.form['password']
+    try:
+        auth.register_user(email=email, password=password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == '__main__':
