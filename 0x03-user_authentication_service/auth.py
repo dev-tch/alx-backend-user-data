68b8a16a30fac5c88a235315086aca74e385b992
dev-tch <tijeni.chaabeni@gmail.com>
2024-05-29 20:00:43 +0000

    generate UUID string

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,7 @@
 import bcrypt
 from db import DB, NoResultFound, InvalidRequestError
 from user import User
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -43,3 +44,7 @@ class Auth:
         except (NoResultFound, InvalidRequestError):
             pass
         return False
+
+    def _generate_uuid() -> str:
+        """return a string representation of a new UUID"""
+        return str(uuid.UUID())
